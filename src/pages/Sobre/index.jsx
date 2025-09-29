cc2a1e96fbc1f15026cf76905727b611e89bfe31
Felipe <FelipeDeangelles@hotmail.com>
2025-09-29 16:38:39 -0300

    continuando

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -1,6 +1,10 @@
 import Cabecalho from "../../Components/Header";
 import Rodape from "../../Components/Rodape";
 
+import { FaLocationDot } from "react-icons/fa6";
+import { IoTime } from "react-icons/io5";
+import { IoIosPhonePortrait } from "react-icons/io";
+
 import psicologa from "./assets/psicologa.jpg"
 
 function Sobre() {
@@ -37,6 +41,20 @@ function Sobre() {
                 <p>“Os pensamentos influenciam emoções, que influenciam comportamentos. Mude seus pensamentos e você mudará sua vida” Aaron Beck.</p>
             </section>
             {/* FIM SEÇÃO VALORES */}
+            {/* SEÇÃO MAPA LOCALIZAÇÃO */}
+            <section className="bg-cyan-900 py-10 flex flex-col justify-center items-center gap-8">
+                <h1 className="text-red-50 text-3xl font-semibold">Contato, Localização e Horários</h1>
+                <div className="flex gap-8 items-center">
+                    <iframe className="" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.1973529954835!2d-46.6564943!3d-23.5613545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59c8da0aa315%3A0xd59f9431f2c9776a!2sAv.%20Paulista%2C%20S%C3%A3o%20Paulo%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1759172657723!5m2!1spt-BR!2sbr" width="600" height="450" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <div className="text-red-50 text-start">
+                        <p className="flex items-center gap-2 font-semibold text-2xl mb-2"><FaLocationDot />Rua 1010, 0100</p>
+                        <p className="flex items-center gap-2 font-semibold text-2xl mb-2"><IoTime />Seg-Sex 08:00 ás 19:00</p>
+                        <p className="flex items-center gap-2 font-semibold text-2xl"><IoIosPhonePortrait />99 99999-1111</p>
+                    </div>
+                </div>
+
+            </section>
+            {/* FIM MAPA LOCALIZAÇÃO */}
             <Rodape></Rodape>
         </>
     )
